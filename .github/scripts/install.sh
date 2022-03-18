5b99918ada498d10a64245cf399aad0069fc67bf
Elias <524433-sailenicolas@users.noreply.gitlab.com>
2022-03-18 13:48:39 -0300

    Added sudo to script

--- .github/scripts/install.sh
+++ .github/scripts/install.sh
@@ -9,10 +9,14 @@ set -xe &&
   sudo apt-get update &&
   sudo apt-get install -y --no-install-recommends ${FETCH_DEPS} &&
   sudo apt-get update
+export PHPVERSIONID=$(ls /usr/include/php) &&
+export arch="$(dpkg-architecture --query DEB_BUILD_GNU_TYPE)" &&
+export multiarch="$(dpkg-architecture --query DEB_BUILD_MULTIARCH)"
 
 RELEASE_ID=$(lsb_release -si)
 # Build PHP library from source.
 PHP_PACKAGES="php8.1-dev php8.1-common php8.1-embed php8.1-cli php8.1-opcache libphp8.1-embed php8.1-readline php8.1-opcache php8.1-xml php8.1 php-common"
+
 if [ "${RELEASE_ID}" == "Debian" ]; then
   curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg &&
     sudo echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" >/etc/apt/sources.list.d/php.list
@@ -29,8 +33,3 @@ sudo apt-get update &&
   export CFLAGS="${PHP_CFLAGS}" CPPFLAGS="${PHP_CPPFLAGS}" LDFLAGS="${PHP_LDFLAGS}"
 sudo apt-get install -y --no-install-recommends ${PHP_PACKAGES}
 sudo ln -sT "/usr/include/php/$(ls /usr/include/php)" /usr/include/php/phpsrc
-export PHPVERSIONID=$(ls /usr/include/php) &&
-  export arch="$(dpkg-architecture --query DEB_BUILD_GNU_TYPE)" &&
-  export multiarch="$(dpkg-architecture --query DEB_BUILD_MULTIARCH)" &&
-  echo 'export PHPVERSIONID=$(ls /usr/include/php)' >>/etc/profile &&
-  . /etc/profile
